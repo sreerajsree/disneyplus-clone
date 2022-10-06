674b7b4f07f630e2b19d8bf2eb271d6d7729f195
sreerajsree <sreeandsreeraj@gmail.com>
2022-10-06 13:20:27 +0530

    header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,17 @@
-import Image from "next/image"
+import Image from "next/image";
+import { HomeIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <div className='sticky bg-[#040714] top-0 z-[1000] flex items-center h-[72px] px-10 md:px-12'>
-        <Image /> 
+    <div className="sticky bg-[#040714] top-0 z-[1000] flex items-center h-[72px] px-10 md:px-12">
+      <Image src="/images/logo.svg" width={80} height={80} className="cursor-pointer" />
+      <div>
+        <a href="/#" className="text-white">
+            <HomeIcon className="h-4" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
